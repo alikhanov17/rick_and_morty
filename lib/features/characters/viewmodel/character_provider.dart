c8865c4fd546c5bb11d85a5b850e7a2405a96fbe
Akobirxuja <108734078+alikhanov17@users.noreply.github.com>
2025-10-09 11:19:40 +0500

    Update character_provider.dart

--- lib/features/characters/viewmodel/character_provider.dart
+++ lib/features/characters/viewmodel/character_provider.dart
@@ -5,15 +5,12 @@ import 'package:rick_and_morty/core/services/api_service.dart';
 import 'package:rick_and_morty/data/repositories/character_repository.dart';
 import 'package:rick_and_morty/features/characters/viewmodel/character_notifier.dart';
 
-// Провайдер для ApiService
 final apiServiceProvider = Provider<ApiService>((ref) => ApiService());
 
-// Провайдер для репозитория
 final characterRepositoryProvider = Provider<CharacterRepository>(
   (ref) => CharacterRepository(ref.read(apiServiceProvider)),
 );
 
-// Провайдер для StateNotifier
 final charactersNotifierProvider =
     StateNotifierProvider<CharactersNotifier, CharactersState>((ref) {
       final repository = ref.read(characterRepositoryProvider);
